439c2df1a008999b187ababf33901d55ab45e5bb
OLWerdffelinir <werdffelynir@gmail.com>
2015-02-10 18:45:34 +0200

    add min docx

--- db/SBuilder.php
+++ db/SBuilder.php
@@ -4,17 +4,35 @@
 namespace db;
 
 
-
 class SBuilder
 {
-
-
+    /** @var SPDO  */
     public $spdo = null;
 
+    /** @var null | \PDOStatement */
+    private $sth = null;
 
+    /** @var null | \PDO */
+    private $dbh = null;
+
+    /** @var string  */
+    private $buildSql = '';
+
+    /** @var array  */
+    private $buildCondition=[];
+
+    /** @var string  */
+    private $prepareType = '';
+
+    /** @var SBuilder | null $instance */
+    private static $instance = null;
+
+    /**
+     * @param bool|string $connectName
+     */
     public function __construct($connectName=false)
     {
-        $this->spdo = new SPDO($connectName);
+        $this->initConnect($connectName);
     }
 
 
@@ -24,12 +42,12 @@ class SBuilder
      */
     public function initConnect($connectName)
     {
-        return $this->spdo->initConnect($connectName);
+        $this->spdo = new SPDO($connectName);
+        $this->spdo->initConnect($connectName);
+        $this->dbh = $this->spdo->dbh();
+
+        return $this->spdo;
     }
-
-
-    /** @var SBuilder | null $instance */
-    private static $instance = null;
 
 
     /**
@@ -54,30 +72,115 @@ class SBuilder
         return $instance->initConnect($connectName);
     }
 
+    /**
+     * @param bool $connectName
+     * @return bool|null|string
+     */
     public function getConnectName($connectName=false)
     {
         return $this->spdo->getConnectName($connectName);
     }
 
 
-
-    public function createCommand($sql, $prepare=[])
+    /**
+     * Get current builder Sql request
+     *
+     * @param bool $withPrepare
+     * @return string
+     */
+    public function getSql($withPrepare=true)
     {
-        return $this->sql;
+        if($withPrepare) {
+            $_sql = "{".$this->buildSql."}\n";
+            foreach((array)$this->buildCondition as $place => $value)
+                $_sql .= 'bind '. ((is_numeric($place)) ? '?'.(string)($place+1) : $place).' = '.$value ."\n";
+            return $_sql;
+        }
+        return $this->buildSql;
     }
 
-    public $sql = '';
 
-    /**
-     * @param $call
-     * @param $params
-     * @return SBuilder
-     */
-    public function __call($call, $params)
-    {
-        $this->sql .= "\n".$call.' '.join(' ',$params);
+    public function select($columns){
+        $this->prepareType = 'SELECT';
+        $this->buildSql .= ' SELECT '.$columns.' ';
         return $this;
     }
+    public function insert($columns){
+        $this->prepareType = 'INSERT';
+        $this->buildSql .= ' INSERT '.$columns.' ';
+        return $this;
+    }
+    public function insertInto($table){
+        $this->prepareType = 'INSERT';
+        $this->buildSql .= ' INSERT INTO '.$table.' ';
+        return $this;
+    }
+    public function update($table){
+        $this->prepareType = 'UPDATE';
+        $this->buildSql .= ' UPDATE '.$table.' ';
+        return $this;
+    }
+    public function delete($table){
+        $this->prepareType = 'DELETE';
+        $this->buildSql .= ' DELETE FROM '.$table.' ';
+        return $this;
+    }
+
+    public function from($table){
+        $this->buildSql .= ' FROM '.$table.' ';
+        return $this;
+    }
+    public function where($condition){
+        $this->buildSql .= ' WHERE '. $condition;
+        return $this;
+    }
+    public function join($table,$condition){
+        $this->buildSql .= ' JOIN '. $table . ' ON ('.$condition.')';
+        return $this;
+    }
+    public function leftJoin($table,$condition){
+        $this->buildSql .= ' LEFT JOIN '. $table . ' ON ('.$condition.')';
+        return $this;
+    }
+    public function innerJoin($table,$condition){
+        $this->buildSql .= ' INNER JOIN '. $table . ' ON ('.$condition.')';
+        return $this;
+    }
+    public function outerJoin($table,$condition){
+        $this->buildSql .= ' OUTER JOIN '. $table . ' ON ('.$condition.')';
+        return $this;
+    }
+    public function rightJoin($table,$condition){
+        $this->buildSql .= ' RIGHT JOIN '. $table . ' ON ('.$condition.')';
+        return $this;
+    }
+    public function orderBy($columns, $sort='ASC') {
+        $this->buildSql .= ' ORDER BY '.$columns.((strtoupper($sort)=='DESC')?' DESC ':' ASC ');
+        return $this;
+    }
+    public function groupBy($columns){
+        $this->buildSql .= ' GROUP BY '.$columns ;
+        return $this;
+    }
+    public function set(array $columns){
+        $this->buildSql .= ' SET '.join(", ",array_map(function($val){return $val."=:".$val;},array_keys($columns)));
+        $this->buildCondition = array_merge($this->buildCondition,$columns);
+        return $this;
+    }
+    public function values(array $columns){
+        $paramsKey = array_keys($columns);
+        $sql = " (".join(", ",$paramsKey).") VALUES(:".join(", :",$paramsKey).") ";
+        $this->buildSql .= $sql;
+        $this->buildCondition = array_merge($this->buildCondition,$columns);
+        return $this;
+    }
+
+
+    public function clear(){
+        $this->buildSql = '';
+        $this->buildCondition = [];
+    }
+
 
     /*
      * Wrapper for PDOStatement::bindValue | PDOStatement::bindParam
@@ -88,38 +191,101 @@ class SBuilder
      * @param int $length
      * @param mixed $driverOptions
      * @return bool|SPDO
-
+     */
     public function bind($parameter, $value, $dataType=\PDO::PARAM_STR, $length=0, $driverOptions=[])
     {
         if($this->sth) {
             $isBind = $this->sth->bindParam($parameter, $value, $dataType, $length, $driverOptions);
             if($isBind)
                 return $this;
+        }else{
+            $this->condition([$parameter=>$value]);
         }
-            return false;
+        return $this;
     }
+
+
+    public function condition(array $condition){
+        $this->buildCondition = array_merge($this->buildCondition,$condition);
+        return $this;
+    }
+
+    /**
+     * Готовит зарпос
+     * @param string $prepare
+     * @return $this
      */
+    public function prepare($prepare='')
+    {
+        if(empty($prepare))
+            $sql = $this->buildSql;
+        else
+            $sql = $prepare;
 
-    public function select($columns){}
-    public function insert($columns){}
-    public function update($columns){}
-    public function delete($columns){}
+        $this->sth = $this->dbh->prepare($sql);
+        return $this;
+    }
 
-    public function from($columns){}
-    public function where($columns){}
-    public function join($columns){}
-    public function leftJoin($columns){}
-    public function innerJoin($columns){}
-    public function outerJoin($columns){}
-    public function rightJoin($columns){}
-    public function orderBy($columns){}
-    public function groupBy($columns){}
-    public function limit($columns){}
-    public function offset($columns){}
-    public function set($columns){}
-    public function values($columns){}
-    public function ____($columns){}
-    public function count($columns){}
+    /**
+     * @param array $condition
+     * @return int|\PDOStatement
+     */
+    public function execute(array $condition=[])
+    {
+        if(!empty($condition))
+            $this->buildCondition = array_merge($this->buildCondition,$condition);
+
+        $buildSql = $this->buildSql;
+        $buildCondition = $this->buildCondition;
+
+        $this->clear();
+
+        if($this->sth != null){
+            $this->sth->execute();
+        }else{
+            $this->sth = $this->spdo->querySql(
+                $buildSql,
+                $buildCondition
+            );
+        }
+
+        # return взависимости от типа запроса
+        if($this->prepareType=='INSERT')
+            return $this->lastInsertId();
+        elseif($this->prepareType=='UPDATE'||$this->prepareType=='DELETE')
+            return $this->rowCount();
+        else
+            return $this->sth;
+    }
+
+    public function executeOne(array $condition=[])
+    {
+        return $this->execute($condition)->fetch();
+    }
+
+    public function executeAll(array $condition=[])
+    {
+        return $this->execute($condition)->fetchAll();
+    }
+
+    /**
+     * Возвращает ид записи после операции запроса INSERT
+     *
+     * примечание в оф.док
+     * @return mixed Returns the ID of the last inserted row or sequence value
+     */
+    public function lastInsertId()
+    {
+        return $this->dbh->lastInsertId();
+    }
+
+    /**
+     * @return int
+     */
+    public function rowCount()
+    {
+        return $this->sth->rowCount();
+    }
 
 }
 
--- test_builder.php
+++ test_builder.php
@@ -10,7 +10,157 @@ use \db\SBuilder;
 # .
 $mdb = new SBuilder('dbMySql');
 
-var_dump($mdb);
+
+/*
+$mdb->select('*')
+    ->from('pages')
+    ->where('id <= :id AND public = :pb')
+    ->condition([
+        ':id'=>1,
+        ':pb'=>1,
+    ]);
+$res = $mdb->execute()->fetchAll();
+*/
+
+/*
+$mdb->select('*')
+    ->from('pages')
+    ->where('id >= :id AND public = :pb')
+    ->prepare()
+        ->bind(':id', '1', \PDO::PARAM_INT)
+        ->bind(':pb', '1', \PDO::PARAM_INT);
+$res = $mdb->execute()->fetchAll();
+*/
+
+/*
+$mdb->select('p.title, p.content, u.name')
+    ->from('pages p')
+    ->leftJoin('users u','u.id = p.userid')
+    ->where('p.public = :pb AND u.id = :uid')
+    ->condition([
+        ':pb'=>1,
+        ':uid'=>2,
+    ]);
+$res = $mdb->execute()->fetchAll();
+*/
+
+/*
+$mdb->insert('pages')
+    ->set([
+        'title'=>'Insert title',
+        'content'=>'Insert content',
+        'userid'=>'1',
+    ]);
+$insertId = $mdb->execute();
+*/
+
+/*
+$mdb->insertInto('pages')
+    ->values([
+        'title'=>'Insert into title',
+        'content'=>'Insert into content',
+        'userid'=>'1',
+    ])
+    ->execute();
+*/
+
+/*
+$mdb->update('pages')
+    ->set([
+        'title'=>'Update builder title',
+        'content'=>'Update builder content',
+        'userid'=>'2',
+    ])
+    ->where('id = :id')
+    ->condition([
+        ':id' => 6
+    ]);
+$updateRows = $mdb->execute();
+*/
+
+/*
+$spdo = $mdb->spdo;
+$insertId = $spdo->queryInsert('pages',[
+    'title' => 'Some title',
+    'content' => 'Some data',
+    'userid' => 2,
+]);
+*/
+
+/*
+$mdb->delete('pages')
+    ->where('id = :id')
+    ->condition([
+        ':id' => 7
+    ]);
+//$updateRows = $mdb->execute();
+*/
+
+/*
+$deletesRows = $mdb->delete('pages')
+    ->where('id = :id')
+    ->execute([
+        ':id' => 8
+    ]);
+var_dump($deletesRows);
+*/
+
+/**
+ * использование bindParam() для этого нужно после построения запроса использувать prepare() без параметров,
+ * таким образом будет создан обект PDOStatement. Если упустить prepare() и дальше использовать bind() все
+ * значения уйдут как параметры в execute(), в место ожидаемого bindParam()
+ *
+$mdb->select('*')
+    ->from('pages')
+    ->where('public = :pb AND userid = :uid')
+    ->prepare()
+        ->bind(':pb', 1, \PDO::PARAM_INT)
+        ->bind(':uid', 2, \PDO::PARAM_INT);
+$res = $mdb->execute()->fetchAll();
+*/
+
+/*
+$res = $mdb
+    ->select('*')
+    ->from('pages')
+    ->where('public = :pb AND userid = :uid')
+    ->orderBy('title','ASC')
+    ->prepare()
+    ->bind(':pb', 1, \PDO::PARAM_INT)
+    ->bind(':uid', 2, \PDO::PARAM_INT)
+    ->executeAll();
+*/
+
+/*
+$mdb->select('*')
+    ->from('pages')
+    ->where('public = :pb AND userid = :uid')
+    ->orderBy('title','ASC');
+
+$res = $mdb->executeAll([
+    ':pb'=>1,
+    ':uid'=>2,
+]);
+*/
+
+/*
+$mdb->select('*')
+    ->from('pages')
+    ->where('public = :pb AND userid = :uid')
+    ->orderBy('title','ASC');
+
+$mdb->condition([
+    ':pb'=>1,
+    ':uid'=>2,
+]);
+
+$res = $mdb->executeAll();
+*/
+
+var_dump($res);
+
+//var_dump($mdb->getSql());
+
 
 /*
 $mdb->select('*')
